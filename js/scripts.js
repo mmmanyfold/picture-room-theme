01b5ca25e75f1e7f55e35c98cdc977ba2daf6223
dviramontes <dviramontes@gmail.com>
2016-06-09 18:25:52 -0600

    begins mustache templating

--- js/scripts.js
+++ js/scripts.js
@@ -36,10 +36,10 @@ $(document).ready(function() {
         // now we can use grid map to render individual images onto
         // the mustache templates..
         var mustache_templates = $('.product-grid-carousel-template');
-        //mustache_templates.each(function (template) {
-          // render grid data onto mustache
-          //$(Mustache.render($(template).html(), data));
-        //});
+        mustache_templates.each(function (index, template) {
+           //render grid data onto mustache
+           //Mustache.render($(template).html(), grid[index]);
+        });
 
     }
   });
