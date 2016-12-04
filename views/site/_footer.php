64288f652b3cc11441464f453121610d5e40627f
eemshi <oreneemshi@gmail.com>
2016-12-04 12:14:57 -0700

    update email address in footer

--- views/site/_footer.php
+++ views/site/_footer.php
@@ -10,7 +10,7 @@
      </div>
      <div class="col-xs-3 col-sm-1 phone-col">
        <strong class="fixed-width-label">T</strong>212 219 2789<br>
-       <strong class="fixed-width-label">E</strong><a href="mailto:info@pictureroom.mcnallyjacksonstore.com">info@</a>
+       <strong class="fixed-width-label">E</strong><a href="mailto:info@mcnallyjacksonstore.com">info@</a>
      </div>
      <div class="col-xs-2 col-sm-1 social-col">
        <strong class="fixed-width-label">I</strong><a href="https://instagram.com/pictureroomnyc/" target="_blank">Instagram</a><br>
